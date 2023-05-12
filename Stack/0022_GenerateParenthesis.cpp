aff4dcb04e0be4101aceb429478e427763de5973
Mano-Liaoyan <ben.jaminlyyf21@gmail.com>
2023-05-12 22:56:51 +0200

    Create 0022

---
+++ Stack/0022_GenerateParenthesis.cpp
@@ -0,0 +1,23 @@
+#include "../LeetDef.hpp"
+
+using namespace std;
+
+/*
+ * @lc app=leetcode.cn id=22 lang=cpp
+ *
+ * [22] 括号生成
+ */
+
+// @lc code=start
+class Solution
+{
+ public:
+  vector<string> generateParenthesis(int n)
+  {
+    stack<char> stk;
+    for (int i = 0; i < n; i++)
+    {
+    }
+  }
+};
+// @lc code=end
