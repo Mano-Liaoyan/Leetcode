e5caca5e4b04259bc1b06ccb6168bc0343a6c2b6
Mano-Liaoyan <ben.jaminlyyf21@gmail.com>
2023-05-15 03:29:12 +0200

    Add car fleet

---
+++ Stack/0853_CarFleet.cpp
@@ -0,0 +1,17 @@
+#include "../LeetDef.hpp"
+
+using namespace std;
+
+/*
+ * @lc app=leetcode.cn id=853 lang=cpp
+ *
+ * [853] 车队
+ */
+
+// @lc code=start
+class Solution
+{
+ public:
+  int carFleet(int target, vector<int>& position, vector<int>& speed) {}
+};
+// @lc code=end
