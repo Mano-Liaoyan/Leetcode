235b27364c21e14a2d2ab90c13e15086f77d8bc7
Mano-Liaoyan <ben.jaminlyyf21@gmail.com>
2023-05-15 15:13:14 +0200

    Finish 0853_CarFleet

--- Stack/0853_CarFleet.cpp
+++ Stack/0853_CarFleet.cpp
@@ -12,6 +12,25 @@ using namespace std;
 class Solution
 {
  public:
-  int carFleet(int target, vector<int>& position, vector<int>& speed) {}
+  int carFleet(int target, vector<int>& position, vector<int>& speed)
+  {
+    int n = position.size();
+    vector<int> id(n);
+    for (int i = 0; i < n; i++) id[i] = i;
+    sort(id.begin(), id.end(), [&](int a, int b) { return position[a] < position[b]; });
+
+    int res = n;
+    double last = 0;
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+      auto t = (target - position[id[i]]) / (double)speed[id[i]];
+      if (t <= last)
+        res--;
+      else
+        last = t;
+    }
+    return res;
+  }
 };
 // @lc code=end
