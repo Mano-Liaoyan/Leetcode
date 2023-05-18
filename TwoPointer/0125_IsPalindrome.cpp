38776090587f9e7c6389189ba9a0cff0f0e84815
Mano-Liaoyan <ben.jaminlyyf21@gmail.com>
2023-05-18 02:51:38 +0200

    Finish 0125 isPalindrome

---
+++ TwoPointer/0125_IsPalindrome.cpp
@@ -0,0 +1,28 @@
+#include "../LeetDef.hpp"
+
+using namespace std;
+
+/*
+ * @lc app=leetcode.cn id=125 lang=cpp
+ *
+ * [125] 验证回文串
+ */
+
+// @lc code=start
+class Solution
+{
+ public:
+  bool check(char c) { return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9'; }
+
+  bool isPalindrome(string s)
+  {
+    for (int i = 0, j = s.size() - 1; i < j; i++, j--)
+    {
+      while (i < j && !check(s[i])) i++;
+      while (i < j && !check(s[j])) j--;
+      if (tolower(s[i]) != tolower(s[j])) return false;
+    }
+    return true;
+  }
+};
+// @lc code=end
