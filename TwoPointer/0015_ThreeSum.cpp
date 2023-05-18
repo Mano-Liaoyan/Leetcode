351485735e3d448a343d3225310de1a7276ac8f7
Mano-Liaoyan <ben.jaminlyyf21@gmail.com>
2023-05-18 16:55:33 +0200

    Finish 0015_ThreeSum

---
+++ TwoPointer/0015_ThreeSum.cpp
@@ -0,0 +1,33 @@
+#include "../LeetDef.hpp"
+
+using namespace std;
+
+/*
+ * @lc app=leetcode.cn id=15 lang=cpp
+ *
+ * [15] 三数之和
+ */
+
+// @lc code=start
+class Solution
+{
+ public:
+  vector<vector<int>> threeSum(vector<int>& nums)
+  {
+    vector<vector<int>> res;
+    sort(nums.begin(), nums.end());
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+      if (i && nums[i] == nums[i - 1]) continue;
+      for (int j = i + 1, k = nums.size() - 1; j < k; j++)
+      {
+        if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+        while (j < k - 1 && nums[i] + nums[j] + nums[k - 1] >= 0) k--;
+        if (nums[i] + nums[j] + nums[k] == 0) res.push_back({nums[i], nums[j], nums[k]});
+      }
+    }
+    return res;
+  }
+};
+// @lc code=end
